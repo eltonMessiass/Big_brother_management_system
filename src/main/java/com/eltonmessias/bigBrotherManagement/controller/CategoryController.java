3f71bcd5e118cbbbbd6dd4d81492e7789cb4e680
eltonMessiass <eltonmessias10@gmail.com>
2024-09-18 16:29:53 +0200

    update: update category

--- src/main/java/com/eltonmessias/bigBrotherManagement/controller/CategoryController.java
+++ src/main/java/com/eltonmessias/bigBrotherManagement/controller/CategoryController.java
@@ -5,10 +5,9 @@ import com.eltonmessias.bigBrotherManagement.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api")
@@ -21,5 +20,11 @@ public class CategoryController {
     public ResponseEntity<CategoryDTO> createCategory(@RequestBody CategoryDTO categoryDTO) {
         CategoryDTO createdCategory = categoryService.createCategory(categoryDTO);
         return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/categories/{id}")
+    public ResponseEntity<CategoryDTO> updateCategory(@PathVariable(value = "id") UUID id, @RequestBody CategoryDTO categoryDTO) throws Exception {
+        CategoryDTO updatedCategory = this.categoryService.updateCategory(id, categoryDTO);
+        return new ResponseEntity<>(updatedCategory, HttpStatus.OK);
     }
 }
--- src/main/java/com/eltonmessias/bigBrotherManagement/service/CategoryService.java
+++ src/main/java/com/eltonmessias/bigBrotherManagement/service/CategoryService.java
@@ -7,11 +7,25 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.UUID;
+
 @Service
 public class CategoryService {
 
     @Autowired
     private CategoryRepository categoryRepository;
+
+    public CategoryDTO convertToDTO(Category category) {
+        CategoryDTO categoryDTO = new CategoryDTO(category.getId(), category.getName(), category.getDescription());
+        BeanUtils.copyProperties(category, categoryDTO);
+        return categoryDTO;
+    }
+
+    public Category convertToEntity(CategoryDTO categoryDTO) {
+        Category category = new Category();
+        BeanUtils.copyProperties(categoryDTO, category);
+        return category;
+    }
 
     public CategoryDTO createCategory(CategoryDTO categoryDTO) {
         Category category = new Category();
@@ -19,5 +33,19 @@ public class CategoryService {
         Category savedCategory = categoryRepository.save(category);
         BeanUtils.copyProperties(savedCategory, categoryDTO);
         return categoryDTO;
+    }
+
+    public CategoryDTO updateCategory(UUID id, CategoryDTO categoryDTO) throws Exception {
+        Category category = categoryRepository.findById(id).orElseThrow(() -> new Exception("Category not found"));
+        if (category != null) {
+//            BeanUtils.copyProperties(categoryDTO, category);
+            category.setName(categoryDTO.name());
+            category.setDescription(categoryDTO.description());
+            Category savedCategory = categoryRepository.save(category);
+//            BeanUtils.copyProperties(savedCategory, categoryDTO);
+            return convertToDTO(savedCategory);
+        } else {
+            throw new Exception("Product not found");
+        }
     }
 }
