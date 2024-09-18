6bd75eb5b270f53e142a93b2551d17f034e90f3e
eltonMessiass <eltonmessias10@gmail.com>
2024-09-18 13:43:34 +0200

    feat: product controller

--- src/main/java/com/eltonmessias/bigBrotherManagement/controller/ProductController.java
+++ src/main/java/com/eltonmessias/bigBrotherManagement/controller/ProductController.java
@@ -2,27 +2,50 @@ package com.eltonmessias.bigBrotherManagement.controller;
 
 
 import com.eltonmessias.bigBrotherManagement.dto.ProductDTO;
+import com.eltonmessias.bigBrotherManagement.model.Product;
 import com.eltonmessias.bigBrotherManagement.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+import java.util.List;
+import java.util.UUID;
+
+@RestController
 @RequestMapping("/api")
 public class ProductController {
 
-    private final ProductService productService;
-
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+    @Autowired
+    private ProductService productService;
 
     @PostMapping("/products")
     public ResponseEntity<ProductDTO> saveProduct(@RequestBody ProductDTO productDTO) {
         ProductDTO createdProduct = productService.saveProduct(productDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/products/{id}")
+    public ResponseEntity<ProductDTO> getProduct(@PathVariable UUID id) {
+        ProductDTO productDTO = productService.getProductById(id);
+        return new ResponseEntity<>(productDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = this.productService.getAllProducts();
+        return new ResponseEntity<>(products, HttpStatus.OK);
+    }
+
+    @PutMapping("/products/{id}")
+    public ResponseEntity<ProductDTO> updateProduct(@PathVariable(value = "id") UUID id, @RequestBody ProductDTO productDTO) throws Exception {
+        ProductDTO updatedProduct = productService.updateProduct(id, productDTO);
+        return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+    }
+
+    @DeleteMapping("products/{id}")
+    public ResponseEntity<ProductDTO> deleteProduct(@PathVariable(value = "id") UUID id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
